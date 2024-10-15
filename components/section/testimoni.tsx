53aecef7ae41e5cc7f0b62bee537e44ea559b339
Forenoo <forenofaisal21@gmail.com>
2024-10-15 20:44:54 +0700

    first commit

---
+++ components/section/testimoni.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Testimoni() {
+  return <div>Testimoni</div>;
+}
---
+++ lib/constants.ts
@@ -0,0 +1,45 @@
+export const navLinksIndonesia = [
+  {
+    id: 1,
+    name: "Beranda",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "Tentang",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Destinasi",
+    href: "#destination",
+  },
+  {
+    id: 4,
+    name: "Ulasan",
+    href: "#rating",
+  },
+] as const;
+
+export const navLinksEnglish = [
+  {
+    id: 1,
+    name: "Home",
+    href: "#home",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "#about",
+  },
+  {
+    id: 3,
+    name: "Destination",
+    href: "#destination",
+  },
+  {
+    id: 4,
+    name: "Rating",
+    href: "#rating",
+  },
+] as const;
